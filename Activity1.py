82803e958c002bc91e41d33aa01597a1b0122f85
BlankQ-max <quest.nathan143@gmail.com>
2024-09-26 10:47:05 +0800

    Our first activity

---
+++ Activity1.py
@@ -0,0 +1,11 @@
+print("Juan")
+num = int(input("Number?: "))
+
+if num >= 99 and <=90
+    print("Galing")
+elif num >= 89 and <=80
+    print("Arang-Arang")
+elif num >= 79 and <=70
+    print("Arang-Arang")
+elif num >= 69 and <=0
+    print("Pangitang Tabang")
