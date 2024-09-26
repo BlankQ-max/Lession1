d65a451fa63f079ce0e3de963504d1161d029957
BlankQ-max <quest.nathan143@gmail.com>
2024-09-26 11:08:46 +0800

    Update

--- Activity1.py
+++ Activity1.py
@@ -1,11 +1,13 @@
 print("Juan")
-num = int(input("Number?: "))
+num = int(input("Grade?: "))
 
-if num >= 99 and <=90
+if num >= 99 and <=90:
     print("Galing")
-elif num >= 89 and <=80
+elif num >= 89 and <=80:
     print("Arang-Arang")
-elif num >= 79 and <=70
+elif num >= 79 and <=70:
     print("Arang-Arang")
-elif num >= 69 and <=0
+elif num >= 69 and <=0:
     print("Pangitang Tabang")
+else:
+    print("Pahuway lang sa")
